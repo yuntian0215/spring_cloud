8854d43e4b035490cb20ef89a49d6871c3ef24ec
lvjie <lvjie@haiwainet.cn>
2019-03-07 14:21:35 +0800

    spring cloud整合eureka服务注册

--- spring_cloud/parent-project/spring-application2/src/main/java/com/yuntian/RunApplication1.java
+++ spring_cloud/parent-project/spring-application2/src/main/java/com/yuntian/RunApplication1.java
@@ -2,6 +2,7 @@ package com.yuntian;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * <p>内容</p>
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author lvjie
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class RunApplication1 {
     public static void main(String[] args) {
         SpringApplication.run(RunApplication1.class,args);
--- spring_cloud/parent-project/spring-eureka-server/src/main/java/com.yuntian/EurekaServer.java
+++ spring_cloud/parent-project/spring-eureka-server/src/main/java/com.yuntian/EurekaServer.java
@@ -2,18 +2,18 @@ package com.yuntian;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * <p>内容</p>
- * 2019/3/1/0001 15:19
+ * 2019/3/7/0007 13:28
  *
  * @author lvjie
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class RunApplication {
+@EnableEurekaServer
+public class EurekaServer {
     public static void main(String[] args) {
-        SpringApplication.run(RunApplication.class,args);
+        SpringApplication.run(EurekaServer.class,args);
     }
 }
